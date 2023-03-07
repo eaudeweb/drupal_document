5ac52ba3d43bf54eeb95d45345bab43b54c39ba1
Anthelixes <dani.eremia@yahoo.com>
2023-03-07 12:56:51 +0200

    Configure the button text with the formatter settings

--- src/Plugin/Field/FieldFormatter/DownloadFileFormatter.php
+++ src/Plugin/Field/FieldFormatter/DownloadFileFormatter.php
@@ -4,6 +4,7 @@ namespace Drupal\drupal_document\Plugin\Field\FieldFormatter;
 
 use Drupal\Component\Serialization\Json;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Url;
 use Drupal\file\Plugin\Field\FieldFormatter\GenericFileFormatter;
@@ -24,6 +25,16 @@ class DownloadFileFormatter extends GenericFileFormatter implements ContainerFac
   /**
    * {@inheritdoc}
    */
+  public static function defaultSettings() {
+    $settings = parent::defaultSettings();
+
+    $settings['button_title'] = 'Download';
+
+    return $settings;
+  }
+  /**
+   * {@inheritdoc}
+   */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $entity = $items->getEntity();
 
@@ -36,7 +47,7 @@ class DownloadFileFormatter extends GenericFileFormatter implements ContainerFac
           'node' => $entity->id(),
           'field_name' => $items->getName(),
         ]),
-        '#title' => $this->t('Download'),
+        '#title' => $this->getSetting('button_title'),
         '#access' => $entity->access('view'),
         '#attributes' => [
           'class' => ['use-ajax', 'download-button'],
@@ -53,6 +64,36 @@ class DownloadFileFormatter extends GenericFileFormatter implements ContainerFac
         ],
       ],
     ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form = parent::settingsForm($form, $form_state);
+
+    $form['button_title'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('The text of the button'),
+      '#required' => TRUE,
+      '#maxlength' => '254',
+      '#default_value' => $this->getSetting('button_title'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = parent::settingsSummary();
+
+    if ($this->getSetting('button_title')) {
+      $summary[] = sprintf('%s: %s', $this->t('The text of the button'), $this->getSetting('button_title'));
+    }
+
+    return $summary;
   }
 
 }
