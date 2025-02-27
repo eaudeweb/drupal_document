227f46c2a81e92a998b77e890e02ed9ce00f9a4a
narcis2005 <chirilov.narcis@yahoo.ro>
2025-02-27 12:49:28 +0200

    Drupal 10 compatible

--- js/documents-form.js
+++ js/documents-form.js
@@ -1,32 +1,34 @@
-(function ($, Drupal) {
 
+(function ($, Drupal, once) {
   'use strict';
 
   Drupal.behaviors.documentsForm = {
     attach: function (context) {
-      $('.documents-bulk-form>table').once('documentsBulkTable')
-          .each(function () {
-            var columns = $(this).find('>thead >tr th').length;
-            var emptyTable = $(this).find('td.views-empty').length !== 0;
-            if (emptyTable) {
-              while (--columns) {
-                // dataTables crashes if number of thead columns != number of tbody columns
-                $(this).find('>tbody >tr').append('<td class=\'hidden\'></td>');
-              }
-            }
-          });
+      once('documentsBulkTable', '.documents-bulk-form>table', context).forEach((table) => {
+        var columns = $(table).find('>thead >tr th').length;
+        var emptyTable = $(table).find('td.views-empty').length !== 0;
+        if (emptyTable) {
+          while (--columns) {
+            // dataTables crashes if number of thead columns != number of tbody columns
+            $(table).find('>tbody >tr').append('<td class=\'hidden\'></td>');
+          }
+        }
+      });
+
       $('.dataTables_wrapper input[type="search"]').on('keyup', function (e) {
         var parentForm = $(this).closest('.documents-bulk-form');
         rebuildFormElements(parentForm);
       });
-      $('.download-documents-clear-button').once('bindToDownloadForm')
-          .on('click', function (event) {
-            event.preventDefault();
-            var parentForm = $(this).closest('form.documents-bulk-form');
-            var checkboxes = parentForm.find('table').find('input[type="checkbox"]');
-            uncheckCheckboxes(checkboxes);
-            rebuildFormElements(parentForm);
-          });
+
+      once('bindToDownloadForm', '.download-documents-clear-button', context).forEach((button) => {
+        $(button).on('click', function (event) {
+          event.preventDefault();
+          var parentForm = $(this).closest('form.documents-bulk-form');
+          var checkboxes = parentForm.find('table').find('input[type="checkbox"]');
+          uncheckCheckboxes(checkboxes);
+          rebuildFormElements(parentForm);
+        });
+      });
 
       function uncheckCheckboxes(checkboxes) {
         checkboxes.each(function () {
@@ -35,28 +37,30 @@
       }
 
       var index = 0;
-      jQuery('.documents-bulk-form table').each(function () {
+      once('selectAllElems', '.documents-bulk-form table', context).forEach((table) => {
         var id = 'select-all-elems_' + index;
         index++;
-        jQuery(this).find('th.select-all input')
-            .attr('id', id)
-            .after('<label for="' + id + '"></label>')
-            .bind('click', function (event) {
-              if (jQuery(event.target).is('input[type="checkbox"]')) {
-                var checkboxes = jQuery(this).closest('table').find('tbody input[type="checkbox"]');
-                checkboxes.each(function () {
-                  var $checkbox = jQuery(this);
-                  var stateChanged = $checkbox.prop('checked') !== event.target.checked;
-                  if (stateChanged) {
-                    $checkbox.prop('checked', event.target.checked).trigger('change');
-                  }
-                  $checkbox.closest('tr').toggleClass('selected', this.checked);
-                });
-                event.stopPropagation();
-              }
-            });
-        jQuery(this).find('input[type="checkbox"]').bind('click', function (event) {
-          var parentForm = jQuery(this).closest('.documents-bulk-form');
+
+        $(table).find('th.select-all input')
+          .attr('id', id)
+          .after('<label for="' + id + '"></label>')
+          .on('click', function (event) {
+            if ($(event.target).is('input[type="checkbox"]')) {
+              var checkboxes = $(this).closest('table').find('tbody input[type="checkbox"]');
+              checkboxes.each(function () {
+                var $checkbox = $(this);
+                var stateChanged = $checkbox.prop('checked') !== event.target.checked;
+                if (stateChanged) {
+                  $checkbox.prop('checked', event.target.checked).trigger('change');
+                }
+                $checkbox.closest('tr').toggleClass('selected', this.checked);
+              });
+              event.stopPropagation();
+            }
+          });
+
+        $(table).find('input[type="checkbox"]').on('click', function () {
+          var parentForm = $(this).closest('.documents-bulk-form');
           rebuildFormElements(parentForm);
         });
       });
@@ -66,31 +70,28 @@
   function rebuildFormElements(parentForm) {
     var selected = parentForm.find('input[type="checkbox"][title!="Deselect all rows in this table"][title!="Select all rows in this table"]:checked');
     var selectedCount = 0;
-    jQuery.each(selected, function (key, element) {
-      var parentType = jQuery(element).parent().parent().parent().prop('nodeName');
+
+    $.each(selected, function (key, element) {
+      var parentType = $(element).parent().parent().parent().prop('nodeName');
       if (parentType !== 'CAPTION' && parentType !== 'DIV' && parentType !== 'THEAD') {
         selectedCount++;
       }
     });
+
     var submitButton = parentForm.find('input[type="submit"]');
     var clearButton = parentForm.find('a.download-documents-clear-button');
     var title = Drupal.t("No documents selected");
+
     if (selectedCount !== 0) {
       title = selectedCount === 1 ? Drupal.t('Download one document') : Drupal.t("Download @count documents", {
         '@count': selectedCount
       });
-      submitButton.attr("value", title);
-      submitButton.attr("disabled", false);
-      submitButton.removeClass("is-disabled");
-      submitButton.removeClass("hidden");
+      submitButton.attr("value", title).attr("disabled", false).removeClass("is-disabled hidden");
       clearButton.removeClass("hidden");
     } else {
-      submitButton.attr("value", title);
-      submitButton.attr("disabled", true);
-      submitButton.addClass("is-disabled");
-      submitButton.addClass("hidden");
+      submitButton.attr("value", title).attr("disabled", true).addClass("is-disabled hidden");
       clearButton.addClass("hidden");
     }
   }
 
-}(jQuery, Drupal));
+}(jQuery, Drupal, once));
